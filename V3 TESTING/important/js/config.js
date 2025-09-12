df7c8c19bcaa6514009650dc7939798e4f96ede8
blake <114522246+blakelol23@users.noreply.github.com>
2025-09-12 09:00:10 -0400

    Add VC endpoint to config.js

--- V3 TESTING/important/js/config.js
+++ V3 TESTING/important/js/config.js
@@ -1,3 +1,3 @@
-﻿
-window.__MS_ENDPOINT__ = 'https://script.google.com/macros/s/AKfycbzGQXAyxv-HowUX-Ulf0-ceZvMbVu8c-jvje982xbxtQOn44i0LqunzcTfT8aHbc-J4TQ/exec';
 
+window.__MS_ENDPOINT__ = 'https://script.google.com/macros/s/AKfycbzGQXAyxv-HowUX-Ulf0-ceZvMbVu8c-jvje982xbxtQOn44i0LqunzcTfT8aHbc-J4TQ/exec';
+window.__VC_ENDPOINT__ = 'https://script.google.com/macros/s/AKfycbyNFNmKjtMrzh7wQMDF81jPE_6JuA9LEYRIYeoYCGETkOstm0OWZYSPptv4MmU6JoXN_Q/exec';
