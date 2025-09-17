d61ec218d46a9e86b8407659d6b06ce4b586b6ac
blake <114522246+blakelol23@users.noreply.github.com>
2025-09-17 14:56:44 -0400

    Delete V3 TESTING directory

--- V3 TESTING/important/js/config.js
+++
@@ -1,7 +0,0 @@
-﻿
-window.__MS_ENDPOINT__ = 'https://script.google.com/macros/s/AKfycbzGQXAyxv-HowUX-Ulf0-ceZvMbVu8c-jvje982xbxtQOn44i0LqunzcTfT8aHbc-J4TQ/exec';
-window.__VC_ENDPOINT__ = 'https://script.google.com/macros/s/AKfycbzgv2NQfnAjK6vi5DOVSIXJBwSUdqYJmD23seWzN6MbeaE5ocMjZrjGGOHQgCl-NoDk/exec';
-
-
-
-// https://script.google.com/macros/s/AKfycbyNFNmKjtMrzh7wQMDF81jPE_6JuA9LEYRIYeoYCGETkOstm0OWZYSPptv4MmU6JoXN_Q/exec?action=getVersion&callback=test
